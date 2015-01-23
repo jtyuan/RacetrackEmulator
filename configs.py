6eb6f56bc0ac61afb789fb55bdfe632d7623069a
jtyuan <ws02298867@gmail.com>
2015-01-23 20:50:30 +0800

    Add configs.py

---
+++ configs.py
@@ -0,0 +1,9 @@
+__author__ = 'bilibili'
+
+CPU_CLOCK = 2 * 2 ** 30  # 2GHz
+CLOCK_CYCLE = 1000       # 1000ticks per cycle
+
+L2_SIZE = 4 * 2 ** 20    # 4MB as default
+L2_ASSOC = 8             # Associativity
+L2_IO_LATENCY = 1        # cycles
+L2_ACCESS_LATENCY = 6    # cycles
